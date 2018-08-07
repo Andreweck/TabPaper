bcf794047a3239ca95f8d5e0eeef319326cdf73d
Andrew <andrew@wiscons.com>
2018-08-07 17:38:43 -0500

    initial commit

---
+++ TabPaper/Controllers/CompositionsController .cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Media;
+
+namespace TabPaper.Controllers
+{
+    public class CompositionsController : Controller
+    {
+        public ActionResult Index()
+        {
+            
+            return View();
+        }
+
+        public ActionResult MusicPaper()
+        {
+            ViewBag.Title = "New Composition";
+            ViewBag.range = "EBGDAEA";
+
+            return View();
+        }
+
+        
+    }
+}
